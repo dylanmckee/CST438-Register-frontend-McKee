c6854306b20e185a20eec25633b12b91ec1ba6ba
McKee <mckdyl16@gmail.com>
2022-10-03 22:07:07 -0700

    Added add student admin functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
+
 import './App.css';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import SchedList from './components/SchedList';
 import Semester from './components/Semester';
+import Admin from './components/Admin';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <AppBar position="static" color="default">
+
         <Toolbar>
+          
            <Typography variant="h6" color="inherit">
             Course Registration
            </Typography>
         </Toolbar>
       </AppBar>
+
       <BrowserRouter>
        <Switch>
-        <Route exact path='/' component={Semester} />
+        <Route exact path='/' component={Semester}  />
+        
         <Route path='/schedule' component={SchedList} />
        </Switch>
       </BrowserRouter>
+      <p>
+      </p>
+      {/* <BrowserRouter>        
+        <Route path='/' component={Admin} />
+      </BrowserRouter> */}
     </div>
   );
 }
---
+++ src/components/AddStudent.js
@@ -0,0 +1,70 @@
+import React, { Component, useState } from 'react';
+import PropTypes from 'prop-types';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+
+// properties addStudent is required, function called when Add clicked.
+// class AddStudent extends Component {
+//       constructor(props) {
+//       super(props);
+//       this.state = {open:false,student:{name:'',email:'',status_code:0 } };
+
+//     };
+    const AddStudent = (props) => {
+        const [open, setOpen] = useState(false);
+        const [student, setStudent] = useState({name:'',email:''});
+    
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    const handleChange = (event) => {
+      setStudent({...student, [event.target.name]: event.target.value});
+      console.log("New value: " , event.target.value);
+      console.log("eventtarget name: ", event.target.name)
+    }
+
+  // Save student and close modal form
+    const handleAdd = () => {
+       props.addStudent(student);
+       console.log("Saved student: ", student.name, student.email);
+       handleClose();
+    }
+
+ 
+      return (
+          <div>
+            <Button variant="outlined" color="primary" style={{margin: 10}} onClick={handleClickOpen}>
+              Add Student
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Add Student</DialogTitle>
+                <DialogContent  style={{paddingTop: 20}} >
+                  <TextField autoFocus fullWidth label="Name" name="name"  onChange={handleChange}  /> 
+                  <p></p>
+                  <TextField autoFocus fullWidth label="Email" name="email"  onChange={handleChange}  /> 
+
+                </DialogContent>
+                <DialogActions>
+                  <Button color="secondary" onClick={handleClose}>Cancel</Button>
+                  <Button id="Add" color="primary" onClick={handleAdd}>Add</Button>
+                </DialogActions>
+              </Dialog>      
+          </div>
+      );
+
+// required property:  addStudent is a function to call to perform the Add action
+// AddStudent.propTypes = {
+//   addStudent : PropTypes.func.isRequired
+// }
+    };
+export default AddStudent;
